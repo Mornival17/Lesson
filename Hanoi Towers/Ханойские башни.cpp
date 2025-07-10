8d0bb0a96588158b71a2d728c482c35d7aa89b5e
Mornival17 <rthbeberf@gmail.com>
2025-07-10 15:42:30 +0000

    Hanoi Towers

---
+++ Hanoi Towers/Ханойские башни.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void hanoy(int n, int start, int ende) {
+	if (n == 1) {
+		std::cout<< "Переместить блинчик " << n << " со столбика " << start << " на столбик " << ende << std::endl;
+	}
+	else {
+		int buf = 6 - start - ende;
+		hanoy(n - 1, start, buf);
+		std::cout << "Переместить блинчик " << n << " со столбика " << start << " на столбик " << ende << std::endl;
+		hanoy(n - 1, buf, ende);
+	}
+}
+
+int main()
+{
+
+	hanoy(3,1,3);
+
+	return 0;
+}
