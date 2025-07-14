a2bfcd8692779169bc3f49b9f0467c265b778114
Mornival17 <rthbeberf@gmail.com>
2025-07-14 10:28:03 +0000

    Simple numbers in bool mass

---
+++ Simple numbers in bool mass /Простые числа в диапозоне, с булевым массивом.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+void diapozon(bool arr[], int n) {
+
+    for (int i = 2; i <= n; i++) {
+        arr[i] = true;
+    }
+
+    for (int i = 2; i * i <= n; i++) {
+        if (arr[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                arr[j] = false;
+            }
+        }
+    }
+
+}
+
+void vavod(bool arr[], int n) {
+
+    for (int y = 2; y <= n; y++) {
+        if (arr[y]) {
+            std::cout << y << " ";
+        }
+    }
+
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    if (n < 2) {
+        std::cout << "Введите число больше 2!" << std::endl;
+        return 0;
+    }
+
+    bool *arr = new bool[n + 1]; 
+
+
+    diapozon(arr, n);
+    vavod(arr, n);
+
+
+    std::cout << std::endl; 
+    delete[] arr; 
+    return 0;
+}
