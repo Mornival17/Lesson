54de7de14d6a3f5514ade012a7d5fcc8b7a052f7
Mornival17 <rthbeberf@gmail.com>
2025-07-28 07:34:10 +0000

    Constructor and Deconstructor

---
+++  Constructor and Deconstructor/Конструктор и деструктор.cpp
@@ -0,0 +1,49 @@
+﻿
+#include <iostream>
+#include <string>
+
+class Person {
+
+private:
+
+    std::string name;
+        int age;
+
+public:
+
+    Person(std::string imia, int vozrast) {
+
+        name = imia;
+        age = vozrast;
+        std::cout << "Вызвался конструктор 321" << std::endl;
+
+    }
+
+    ~Person() {
+
+        std::cout << "Вызвался деструктор 123" << std::endl;
+
+    }
+
+    void print() {
+
+        std::cout << "Имя: " << name << std::endl;
+        std::cout << "Возраст: " << age << std::endl;
+
+    }
+
+};
+
+int main()
+{
+
+    setlocale(LC_ALL, "ru");
+    
+    Person person("Мария", 23);
+
+    person.print();
+
+    return 0;
+
+}
+
