00c5678c2f781afd1ddaa97abc306d4529166f6e
Mornival17 <rthbeberf@gmail.com>
2025-07-29 08:44:15 +0000

    Use switch_case

---
+++  Use switch_case/Использование конструкции switch_case.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+int abrabochik(int a, int b) {
+
+    char g;
+    std::cout << "Введите значение операцию: ";
+    std::cin>>g;
+
+    switch (g)
+    {
+    case '+': {
+        std::cout << a << " + " << b << " = " << a + b << std::endl;
+        break;
+    }
+    case '-': {
+        std::cout << a << " - " << b << " = " << a - b << std::endl;
+        break;
+    }
+    case '*': {
+        std::cout << a << " * " << b << " = " << a * b << std::endl;
+        break;
+    }
+    case '/': {
+        std::cout << a << " / " << b << " = " << a / b << std::endl;
+        break;
+    }
+    default:
+        std::cout << "Ты даун!" << std::endl;
+        break;
+     }
+    return 0;
+}
+
+
+int main()
+{
+
+    int a, b;
+
+    std::cout << "Введите значение а: ";
+    std::cin >> a;
+
+    std::cout << "Введите значение b: ";
+    std::cin >> b;
+
+    abrabochik(a, b);
+
+    return 0;
+}
+
