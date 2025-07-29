a6e49f7bbbb1f4612b2ac58c4a113d3b087cdade
Mornival17 <rthbeberf@gmail.com>
2025-07-29 15:43:27 +0000

    Revers_word

---
+++ Revers_word/Реверс слова.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+void revers(std::string slovo) {
+	
+	int dlina = slovo.length();
+
+	for (int i = dlina - 1; i >= 0; i--) {
+		std::string clov(1, slovo[i]);
+		std::cout << clov;
+	}
+
+	}
+
+
+int main()
+{
+
+	std::string slovo;
+	std::cin >> slovo;
+
+	revers(slovo);
+
+
+	return 0;
+}
