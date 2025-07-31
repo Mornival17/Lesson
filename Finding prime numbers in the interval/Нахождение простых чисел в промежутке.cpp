2dd381b0a8fbf2d20cb384c1fe208a53e5676bc1
Mornival17 <rthbeberf@gmail.com>
2025-07-31 07:18:03 +0000

    Finding prime numbers in the interval

---
+++ Finding prime numbers in the interval/Нахождение простых чисел в промежутке.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+void diapozon(int a, int b) {
+
+    for (int i = a; i < b; i++) {
+
+        bool g = true;
+
+        for (int j = 2; j <= i / 2; j++) {
+
+            if ((i % j) == 0) {
+
+                g = false;
+
+                break;
+
+            }
+        }
+
+        if (g) {
+
+            std::cout << i << " ";
+
+        }
+    }
+
+}
+
+int main()
+{
+    
+    int a, b;
+
+    std::cout << "Введите нижyюю границу промежутка: ";
+    std::cin >> a;
+
+    std::cout << "Введите верхнюю границу промежутка: ";
+    std::cin >> b;
+
+    diapozon(a, b);
+
+    return 0;
+
+}
+
